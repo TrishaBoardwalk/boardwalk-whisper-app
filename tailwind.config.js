e8b24abc5bd97e6c058841714db1a270774aaa6f
TrishaBoardwalk <trisha@boardwalkaruba.com>
2026-01-30 12:55:48 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        boardwalk: {
+          turquoise: '#00b3c2',
+          beige: '#f2ebe2',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
